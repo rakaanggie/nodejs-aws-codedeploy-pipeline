8223a79ddab3b8abb0620ebe1d76a2b938697747
rakaanggie <114036721+rakaanggie@users.noreply.github.com>
2023-09-29 18:43:48 +0700

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
 echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-pm2 restart app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+pm2 restart default >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
